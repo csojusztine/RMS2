5d1acc7662090ffcccc90f042bea97a755391a38
csojusztine <juszti27@gmail.com>
2020-10-30 20:16:31 +0100

    Home és User componentek létrehozva

--- src/main/frontend/frontend/src/app/app-routing.module.ts
+++ src/main/frontend/frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'users', component: UserComponent},
+  {path: '', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/main/frontend/frontend/src/app/app.module.ts
+++ src/main/frontend/frontend/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
