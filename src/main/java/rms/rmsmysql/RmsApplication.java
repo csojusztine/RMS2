96f4573b894433618da9b5c76debeefed45e2afb
csojusztine <juszti27@gmail.com>
2020-10-30 18:41:37 +0100

    Műkszik a szaros Angular alap

--- src/main/java/rms/rmsmysql/RmsApplication.java
+++ src/main/java/rms/rmsmysql/RmsApplication.java
@@ -3,9 +3,7 @@ package rms.rmsmysql;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
+
 
 @SpringBootApplication
 public class RmsApplication {
