1d16a91dbf460a63055a800e2b2a7aaf5e2baa10
csojusztine <juszti27@gmail.com>
2020-11-08 18:17:34 +0100

    User listet ki lehet iratni végre

--- src/main/java/rms/rmsmysql/security/MyUserDetailsService.java
+++ src/main/java/rms/rmsmysql/security/MyUserDetailsService.java
@@ -1,31 +1,38 @@
 package rms.rmsmysql.security;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import rms.rmsmysql.entities.User;
+import rms.rmsmysql.entities.enums.Role;
 import rms.rmsmysql.repository.UserRepository;
 
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 
 @Service
-public class MyUserDetailsService implements UserDetailsService {
+public class MyUserDetailsService implements UserDetailsService//, ApplicationListener<AuthenticationSuccessEvent>
+{
 
     @Autowired
     private UserRepository userRepository;
+
+    private static final Logger logger = LoggerFactory.getLogger(MyUserDetailsService.class);
 
     @Autowired
     private AuthenticatedUser authenticatedUser;
 
     @Override
-    @Transactional(readOnly = true)
+    @Transactional(propagation = Propagation.REQUIRED, readOnly = false)
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Optional<User> oUser = userRepository.findByUsername(username);
         if (!oUser.isPresent()) {
@@ -38,4 +45,39 @@ public class MyUserDetailsService implements UserDetailsService {
 
         return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), grantedAuthorities);
     }
+
+    /*@Override
+    public void onApplicationEvent(AuthenticationSuccessEvent event) {
+        UserDetails principal = (UserDetails) event.getAuthentication().getPrincipal();
+        String username = principal.getUsername();
+        logger.info("User successfully logged in, username: {}", username);
+        List<String> roles = new ArrayList<>();
+        principal.getAuthorities().forEach(o -> {
+            String authority = o.getAuthority();
+            roles.add(authority);
+        });
+        logger.info("User's authorities: {}", roles);
+        Optional<User> byUsername = userRepository.findByUsername(username);
+        if (byUsername.isPresent()) {
+            User existingPerson = byUsername.get();
+            Role newRole = detectRole(roles);
+            if (!newRole.equals(existingPerson.getRole())) {
+                existingPerson.setRole(newRole);
+                userRepository.save(existingPerson);
+            }
+        } else {
+            User newPerson = new User();
+            newPerson.setUsername(username);
+            newPerson.setRole(detectRole(roles));
+            userRepository.save(newPerson);
+        }
+
+    }
+    private Role detectRole(List<String> roles) {
+        if (roles.contains("ROLE_ADMIN")) {
+            return Role.ROLE_ADMIN;
+        } else {
+            return Role.ROLE_WORKER;
+        }
+    }*/
 }
