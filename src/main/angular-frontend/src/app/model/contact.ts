004a0fafa9d6049985ff2da76665c78f97b693de
csojusztine <juszti27@gmail.com>
2021-04-27 23:26:56 +0200

    Confirmation email küldve, már csak az url kéne hogy működjön, hogy elfogadom a cuccost

---
+++ src/main/angular-frontend/src/app/model/contact.ts
@@ -0,0 +1,12 @@
+import { Work } from "./work";
+
+export class Contact {
+
+    manufacturer: string;
+    type:string;
+    description: string;
+    price: number;
+    customers_email: string;
+    note: string;
+    work: Work;
+}
